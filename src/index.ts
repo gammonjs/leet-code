1fabad44c103e7a6c1c389bc04451a22033a093c
Joseph <gammon.joseph@gmail.com>
2021-08-08 21:15:27 -0500

    feat: final matrix transform efforts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 // import { minDeletions } from './microsoft/character-frequencies';
 
+import { matrixRankTransform } from './week-2-august-8th-august-14th/matrix-rank-transform';
+
 // console.log(minDeletions("aaabbbcc"))
+
+console.log(
+    matrixRankTransform([
+        [20, -21, 14],
+        [-19, 4, 19],
+        [22, -47, 24],
+        [-19, 4, 19]
+    ])
+);
--- src/week-2-august-8th-august-14th/matrix-rank-transform.ts
+++ src/week-2-august-8th-august-14th/matrix-rank-transform.ts
@@ -19,15 +19,146 @@
     -109 <= matrix[row][col] <= 109
  */
 
-export function matrixRankTransform(matrix: number[][]): number[][] {
-    // Sort the cells by value and process them in increasing order.
+// 9 / 39 test cases passed
 
-    // The rank of a cell is the maximum rank in its row and column plus one.
+interface ICell {
+    row: number;
+    col: number;
+    value: number;
+}
 
-    // Handle the equal cells by treating them as components using a union-find data structure.
+interface INode {
+    value: number;
+    rank: number;
+    children: Array<INode>;
+}
 
-    return [
-        [1, 2],
-        [2, 3]
+class Graph {
+    private readonly _map = new Map<number, INode>();
+
+    node = (value: number): INode => {
+        const result = this._map.get(value);
+
+        if (result) {
+            return result;
+        }
+
+        this._map.set(value, <INode>{
+            value: value,
+            rank: 1,
+            children: Array<INode>()
+        });
+
+        return this._map.get(value) as INode;
+    };
+}
+
+const ascending = (a: ICell, b: ICell) => (a.value > b.value ? 1 : -1);
+
+class SortedMatrixCells {
+    private _lazy: Array<ICell> | null;
+    constructor(private readonly _matrix: Array<Array<number>>) {
+        this._lazy = null;
+    }
+
+    cells = (): Array<ICell> => {
+        if (this._lazy) {
+            return this._lazy;
+        }
+
+        const cells: ICell[] = [];
+
+        this._matrix.forEach((row: number[], idy) =>
+            row.forEach((cell: number, idx) => {
+                cells.push(<ICell>{
+                    row: idy,
+                    col: idx,
+                    value: cell
+                });
+            })
+        );
+
+        this._lazy = cells.sort(ascending);
+        return this._lazy;
+    };
+
+    rows = (cell: ICell) => [
+        ...new Set(
+            this._lazy
+                ?.filter((x: ICell) => x.row === cell.row)
+                .sort(ascending)
+                .map((x) => x.value)
+        )
+    ];
+
+    cols = (cell: ICell) => [
+        ...new Set(
+            this._lazy
+                ?.filter((x: ICell) => x.col === cell.col)
+                .sort(ascending)
+                .map((x) => x.value)
+        )
     ];
 }
+
+export function matrixRankTransform(matrix: number[][]): number[][] {
+    const sorted = new SortedMatrixCells(matrix);
+
+    const graph = new Graph();
+
+    sorted.cells().forEach((cell: ICell) => {
+        const rows = sorted.rows(cell);
+        const cols = sorted.cols(cell);
+
+        for (let i = 0; i < rows.length - 1; i++) {
+            const parent = graph.node(rows[i]);
+            const child = graph.node(rows[i + 1]);
+
+            if (
+                parent?.children.filter((x) => x.value === rows[i + 1])
+                    .length === 0
+            ) {
+                parent?.children.push(child);
+            }
+        }
+
+        for (let i = 0; i < cols.length - 1; i++) {
+            const parent = graph.node(cols[i]);
+            const child = graph.node(cols[i + 1]);
+
+            if (
+                parent?.children.filter((x) => x.value === cols[i + 1])
+                    .length === 0
+            ) {
+                parent?.children.push(child);
+            }
+        }
+    });
+
+    sorted.cells().forEach((x) => {
+        const parent = graph.node(x.value);
+
+        parent.children.forEach((child) => {
+            if (parent.rank >= child.rank) {
+                child.rank = parent.rank + 1;
+            }
+        });
+    });
+
+    matrix.forEach((row, idy) => {
+        row.forEach((cell, idx) => {
+            matrix[idy][idx] = graph.node(matrix[idy][idx]).rank;
+        });
+    });
+
+    return matrix;
+}
+
+/*
+        Handle the equal cells by treating them as components using a union-find data structure.
+        https://algorithmist.com/wiki/Union_find
+
+        Union Find is an algorithm which uses a disjoint-set data structure to solve the following problem: Say we have some number of items. 
+        We are allowed to merge any two items to consider them equal (where equality here obeys all of the properties of an Equivalence Relation). 
+        At any point, we are allowed to ask whether two items are considered equal or not.
+     */
