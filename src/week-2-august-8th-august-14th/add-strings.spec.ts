a39a1f15db14b363ce398c3f5463eb2eb1518833
Joseph <gammon.joseph@gmail.com>
2021-08-09 19:57:36 -0500

    feat: addStrings acceptance testing

---
+++ src/week-2-august-8th-august-14th/add-strings.spec.ts
@@ -0,0 +1,18 @@
+import { addStrings } from "./add-strings";
+
+// this was a pretty simple problem so no other testing was necessary for acceptance
+
+describe('function addStrings(num1: string, num2: string): string', () => {
+    it('returns "134" for inputs "11" and "123', () => {
+        // arrange
+        const num1: string = "11";
+        const num2: string = "123";
+        const expected: string = "134";
+
+        //act
+        const actual: string = addStrings(num1, num2);
+
+        //assert
+        expect(actual).toBe(expected);
+    });
+})
